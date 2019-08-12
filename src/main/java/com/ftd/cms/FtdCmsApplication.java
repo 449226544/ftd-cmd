eb84fcdbb341f54f92157547ac429a4dd92bc4e0
WangJixin <449226544@qq.com>
2019-08-12 14:55:54 +0800

    init

---
+++ src/main/java/com/ftd/cms/FtdCmsApplication.java
@@ -0,0 +1,13 @@
+package com.ftd.cms;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FtdCmsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FtdCmsApplication.class, args);
+    }
+
+}
